d83b6325e85777960771dc1294caf47c64365d1a
Yao Jiang <yaojiang7595@gmail.com>
2022-07-27 13:24:49 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Wow! Hello there, ${req.params.name}!`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  res.send(
+    `Tip = ${
+      (parseInt(req.params.total) * parseInt(req.params.tipPercentage)) / 100
+    }`
+  );
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
